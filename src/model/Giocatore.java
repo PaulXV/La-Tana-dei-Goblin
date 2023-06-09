e88c39f3cda1afbce55074b6e619db04e61c1b87
paolo.brugnola <113890106+PaulXV@users.noreply.github.com>
2023-06-09 11:49:51 +0200

    creazione progetto
    project

---
+++ src/model/Giocatore.java
@@ -0,0 +1,9 @@
+package model;
+import java.util.*;
+
+public class Giocatore
+{
+	private String nome;
+	private String nickname;
+	private HashMap<String, Gioco> giochi = new HashMap<String, Gioco>();
+}
