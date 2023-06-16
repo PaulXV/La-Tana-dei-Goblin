99f3f85f581727633cdb9bdf3e4cdb0edd194d89
paolo.brugnola <113890106+PaulXV@users.noreply.github.com>
2023-06-16 17:09:40 +0200

    added menu
    first menu idea with two buttons

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -4,14 +4,17 @@ import javax.swing.*;
 import model.*;
 
 public class GUI extends JFrame{
-
+	
 	GUI(){
-		this.setBackground(new Color(228,228,228));
+		
 		this.setLayout(new BorderLayout());
-		this.setTitle("La Tana dei Goblin"); //TODO	
+		this.setTitle("La Tana dei Goblin"); //TODO
 		this.setSize(1200, 800);
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
+		
+		MenuGui m = new MenuGui();
+		this.add( m, BorderLayout.CENTER);
 		
 		this.setVisible(true);
 	}
---
+++ src/gui/MenuGui.java
@@ -0,0 +1,99 @@
+package gui;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.*;
+import javax.swing.border.*;
+
+public class MenuGui extends JPanel{
+	
+	private Border border = BorderFactory.createLineBorder(Color.white);
+	private Color text_color = new Color(224,224,224);
+	private Color hover = new Color(44,132,0);
+	private Color background = new Color(51,102,0);
+	private Cursor pointer = new Cursor(12);
+	private Font btn_font = new Font("Serif", Font.BOLD, 30);
+	private Font title_font = new Font("Serif", Font.BOLD, 60);
+	
+	MenuGui(){
+		this.setBackground(new Color(51, 102, 0));
+		this.setLayout(new GridLayout(0,1));
+		
+		
+		JLabel title = new JLabel("La Tana dei Goblin");
+		title.setFont(title_font);
+		title.setForeground(text_color);
+		title.setHorizontalAlignment(0);
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setBackground(background);
+		buttonPanel.setAlignmentY(0);
+		buttonPanel.setAlignmentX(0);
+		
+		JButton login = new JButton("  LOGIN  ");
+		//TODO: quando click su login fare schermata di login nel portale
+		//TODO: quando click su singin fare schermata di singin nel portale;
+		//gestire solo il mouse clicked - se pressed chiama mouseClicked();
+		//dopo l'accesso devi far vedere o schermata autori o schermata giocatori
+		
+		login.addMouseListener(new MouseListener() {
+			
+			public void mouseClicked(MouseEvent e) {
+				
+			}
+			public void mousePressed(MouseEvent e) {mouseClicked(e);}
+			public void mouseEntered(MouseEvent e) {}
+			public void mouseExited(MouseEvent e) {}
+			public void mouseReleased(MouseEvent e) {}
+		});
+		
+		JButton singIn = new JButton(" SING IN ");
+		singIn.addMouseListener(new MouseListener() {
+			
+			public void mouseClicked(MouseEvent e) {
+				
+			}
+			public void mousePressed(MouseEvent e) {mouseClicked(e);}
+			public void mouseExited(MouseEvent e) {}
+			public void mouseEntered(MouseEvent e) {}
+			public void mouseReleased(MouseEvent e) {}
+		});
+		
+		FlowLayout flow = new FlowLayout(0,510,35);
+		buttonPanel.setLayout(flow);
+		
+		styleBtn(login);
+		styleBtn(singIn);
+		buttonPanel.add(login);
+		buttonPanel.add(singIn);
+		
+		this.add(title);
+		this.add(buttonPanel);
+	}
+	
+	private void styleBtn(JButton btn) {
+		btn.setFont(btn_font);
+		btn.setFocusable(false);
+		btn.setBorderPainted(true);
+		btn.setBorder(border);
+		btn.setBackground(background);
+		btn.setForeground(text_color);
+		btn.setMargin( new Insets(3,3,6,6) );
+		btn.setCursor(pointer);
+		btn.setRolloverEnabled(true);
+		
+		btn.addMouseListener(new java.awt.event.MouseAdapter() {
+			public void mouseEntered(java.awt.event.MouseEvent e) {
+				btn.setBackground(hover);
+				btn.setForeground(Color.white);
+			}
+			
+			public void mouseExited(java.awt.event.MouseEvent e) {
+				btn.setBackground(background);
+				btn.setForeground(text_color);
+			}
+		});
+	}
+	
+}
